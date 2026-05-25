3b08bda65864dcf934769ac327733b0c68670ffb
bhadra3156 <lifeofbhadra@gmail.com>
2026-05-25 18:42:46 +0100

    Fix CORS - allow all origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000","http://localhost:3001","http://localhost:3002","https://*.vercel.app"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
